4872aa4fbc61e79fbe585d388ebc61d2b679f20f
zhymas <shvachko319@gmail.com>
2025-07-27 22:51:06 +0300

    Add default value

--- src/config/config.py
+++ src/config/config.py
@@ -8,18 +8,18 @@ ROOT_DIR = Path(__file__).parent.parent.parent
 load_dotenv(ROOT_DIR / ".env")
 
 class DatabaseConfig(BaseSettings):
-    db_username: str = Field(..., env="DB_USERNAME")
-    db_password: str = Field(..., env="DB_PASSWORD")
-    db_host: str = Field(..., env="DB_HOST")
-    db_port: int = Field(..., env="DB_PORT")
-    db_name: str = Field(..., env="DB_NAME")
+    db_username: str = Field(default="postgres", env="DB_USERNAME")
+    db_password: str = Field(default="postgres", env="DB_PASSWORD")
+    db_host: str = Field(default="localhost", env="DB_HOST")
+    db_port: int = Field(default=5432, env="DB_PORT")
+    db_name: str = Field(default="postgres", env="DB_NAME")
 
     @property
     def url(self) -> str:
         return f"postgresql+asyncpg://{self.db_username}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
     
 class CountriesConfig(BaseSettings):
-    wikipedia_url: str = Field(..., env="WIKIPEDIA_URL")
+    wikipedia_url: str = Field(default="https://en.wikipedia.org/w/index.php?title=List_of_countries_by_population_(United_Nations)&oldid=1215058959", env="WIKIPEDIA_URL")
 
 class Config(BaseSettings):
     database: DatabaseConfig = DatabaseConfig()
